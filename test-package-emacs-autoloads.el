8be4df19efafb782a240054eb33c1924d6ae353c
Cletip <clement020302@gmail.com>
2022-12-12 22:36:51 +0100

    moddif autoload voir si straight automatique

--- test-package-emacs-autoloads.el
+++
@@ -1,35 +0,0 @@
-;;; test-package-emacs-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "test-package-emacs" "test-package-emacs.el"
-;;;;;;  "f3cdaba1c93381f2607fdc365ccdca82")
-;;; Generated autoloads from test-package-emacs.el
-
-(autoload 'test-package-emacs-name-of-function "test-package-emacs" "\
-DOCSTRING here" t nil)
-
-(autoload 'test-package-emacs-ask-name "test-package-emacs" "\
-Ask name.
-
-\(fn X)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "test-package-emacs"
-;;;;;;  "test-package-emacs.el" (25495 39046 182282 128000))
-;;; Generated autoloads from test-package-emacs.el
-
-(register-definition-prefixes "test-package-emacs" '("unevariable"))
-
-;;;***
-
-;;;***
-
-(provide 'test-package-emacs-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; test-package-emacs-autoloads.el ends here
